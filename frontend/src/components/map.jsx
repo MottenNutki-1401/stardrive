fa2d3bcdda75d65bbed8d9a8bea5c4049e613a9c
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-05-06 02:58:41 +0800

    remove secreet key

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -1,13 +1,15 @@
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 function Map() {
+  const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
   const center = {
-    lat: 14.8386,  
+    lat: 14.8386,
     lng: 120.2842,
   };
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyDt1rXiJxNUOHdAX6i-BKkYal4ZPI-1afA">
+    <LoadScript googleMapsApiKey={API_KEY}>
       <GoogleMap
         mapContainerStyle={{ width: "100%", height: "100%" }}
         center={center}
--- frontend/src/pages/PassengerHome.jsx
+++ frontend/src/pages/PassengerHome.jsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import Map from "../components/Map";
 import "../styles/passengerhome.css";
 
 function PassengerHome() {
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+
   return (
     <div className="passenger-page">
+
+      {/* MAP */}
       <Map />
+
+      {/* OVERLAY UI */}
+      <div className="overlay">
+
+        <input
+          type="text"
+          placeholder="Add a pickup location"
+          value={pickup}
+          onChange={(e) => setPickup(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Add a destination"
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
+        />
+
+        <button onClick={() => console.log(pickup, destination)}>
+          Get Fare
+        </button>
+
+      </div>
+
     </div>
   );
 }
